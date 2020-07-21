c188d94fa8f39c29bc3383d96b1bd5f931dfd000
umakiran9 <umakiran9@github.com>
2020-07-21 20:49:06 +0530

    final commit

---
+++ anmation.js
@@ -0,0 +1,14 @@
+var textWrapper = document.querySelector('.ml7 .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline()
+  .add({
+    targets: '.ml7 .letter',
+    translateY: ["1.1em", 0],
+    translateX: ["0.55em", 0],
+    translateZ: 0,
+    rotateZ: [180, 0],
+    duration: 750,
+    easing: "easeOutExpo",
+    delay: (el, i) => 50 * i
+  })
